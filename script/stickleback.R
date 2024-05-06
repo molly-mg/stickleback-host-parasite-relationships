c2cc9a1603d754d868d262347860943559ef5a07
molly-mg <mmgood2222@gmail.com>
2024-05-06 11:32:09 +0000

    Loading the data

---
+++ script/stickleback.R
@@ -0,0 +1,11 @@
+# PACKAGES ----
+library(usethis)
+library(tidyverse)
+#_________________----
+
+# LOADING DATA ----
+stickleback <- read_csv("data/parasite_exp.csv")
+
+head(stickleback)
+#_________________----
+
